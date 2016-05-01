3b8363e6685f6ca1b71b05414f3ab9c3a9d1ca54
invaderDev <kristiforpresident@gmail.com>
2016-05-01 19:10:55 +0000

    first push

---
+++ lib/calculator.rb
@@ -0,0 +1,48 @@
+class Calculator
+    def initialize(input_array)
+        @input_array = []
+    end
+    
+  def get_input
+    puts "Please enter a valid mathematical equation with positive integers (i.e. 2 + 2):"
+    input = gets.chomp
+    
+    fill_input_array(input)
+  
+    if valid_input?
+      puts "The result is **#{calculate}**"
+      get_input
+    else
+      puts "Invalid input. Please try again."
+      get_input
+    end
+  end
+
+  def fill_input_array(input)
+    @input_array = []
+    input.delete(" ").each_char do |i| 
+        @input_array << i
+    end
+  end
+
+  def calculate
+    @input_array[0].to_f.send(@input_array[1], @input_array[2].to_f)
+  end
+
+  def valid_input?
+    operand? && valid_ints? && length?
+  end
+  
+  def operand?
+    @input_array[1] == "+" || @input_array[1] == "-" || @input_array[1] == "*" || @input_array[1] == "/"
+  end
+  
+  def valid_ints?
+    @input_array[0].to_f > 0 && @input_array[2].to_f > 0
+  end
+  
+  def length?
+    @input_array.length == 3
+  end
+  
+end
